027e01ef4eb1f26e8115435cf4a2473fcf241756
Bozho <bozhidar.bozhanov@gmail.com>
2018-06-14 17:04:36 +0300

    null or empty check

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/controller/ValidationController.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/controller/ValidationController.java
@@ -27,6 +27,7 @@ import javax.validation.Valid;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -133,7 +134,7 @@ public class ValidationController {
 	
 	@PostConstruct
 	public void init() throws IOException, CertificateException {
-	    if (signingCertificateJksPath == null) {
+	    if (StringUtils.isEmpty(signingCertificateJksPath)) {
 	        return;
 	    }
 	    
